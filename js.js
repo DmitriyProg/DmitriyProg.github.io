20c52b94896f100be96d0519d4e862e861c2821a
DmitriyProg <139871016+DmitriyProg@users.noreply.github.com>
2023-07-26 12:48:19 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,4 @@
-// Это объявление переменной, мы наши кнопку по тегу 
 const button = document.querySelector('button');
-
-// Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику 
 button.addEventListener('click', function() {
 	alert('Дорогу осилит идущий (^v^) ')
 })
